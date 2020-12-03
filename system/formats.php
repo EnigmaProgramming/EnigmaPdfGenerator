eee4f5348397126aa7badcd765d8e2883807b772
Mederick Bernier <mederick.bernier@hotmail.ca>
2020-12-03 08:30:23 -0500

    modified the formats.php file by removing the '-L' that were added in the wrong place

--- system/formats.php
+++ system/formats.php
@@ -2,28 +2,28 @@
 
 class PdfFormats{
     #region Normal Formats
-    const F_A0 = "A0-L";
-    const F_A10 = "A10-L";
-    const F_B0 = "B0-L";
-    const F_B10 = "B10-L";
-    const F_C0 = "C0-L";
-    const F_C10 = "C10-L";
-    const F_4A0 = "4A0-L";
-    const F_2A0 = "2A0-L";
-    const F_RA0 = "RA0-L";
-    const F_RA4 = "RA4-L";
-    const F_SRA0 = "SRA0-L";
-    const F_SRA4 = "SRA4-L";
-    const F_LETTER = "Letter-L";
-    const F_LEGAL = "Legal-L";
-    const F_EXECUTIVE = "Executive-L";
-    const F_FOLIO = "Folio-L";
-    const F_DEMY = "Demy-L";
-    const F_ROYAL = "Royal-L";
-    const F_A = "A-L";
-    const F_B = "B-L";
-    const F_LEDGER = "Ledger-L";
-    const F_TABLOID = "Tabloid-L";
+    const F_A0 = "A0";
+    const F_A10 = "A10";
+    const F_B0 = "B0";
+    const F_B10 = "B10";
+    const F_C0 = "C0";
+    const F_C10 = "C10";
+    const F_4A0 = "4A0";
+    const F_2A0 = "2A0";
+    const F_RA0 = "RA0";
+    const F_RA4 = "RA4";
+    const F_SRA0 = "SRA0";
+    const F_SRA4 = "SRA4";
+    const F_LETTER = "Letter";
+    const F_LEGAL = "Legal";
+    const F_EXECUTIVE = "Executive";
+    const F_FOLIO = "Folio";
+    const F_DEMY = "Demy";
+    const F_ROYAL = "Royal";
+    const F_A = "A";
+    const F_B = "B";
+    const F_LEDGER = "Ledger";
+    const F_TABLOID = "Tabloid";
     #endregion
     
     #region Landscape Formats
