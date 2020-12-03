d1be097cc7984c40261d7fdcc211078026f416a3
Mederick Bernier <mederick.bernier@hotmail.ca>
2020-12-03 02:10:16 -0500

    added the config file

---
+++ configs/config.php
@@ -0,0 +1,181 @@
+<?php
+
+// this file will contain the configurations for the pdf parser as global values.
+// the values are set to the default then commented, to activate a specific config, just remove the comment and set the value to the one you want to use.
+
+#region Paging
+//define("mirrorMargins",0);
+//define("forcePortraitMargins",false);
+//define("displayDefaultOrientation",false);
+//define("autoPageBreak",true);
+//define("setAutoTopMargin",false);
+//define("setAutoBottomMargin",false);
+//define("autoMarginPadding",2);
+//define("margBuffer",2);
+//define("printers_info",false);
+//define("bleedMargin",5);
+//define("crossMarkMargin",5);
+//define("cropMarkMargin",8);
+//define("nonPrintMargin",8);
+//define("defaultPagebreakType",'cloneall');
+#endregion
+
+#region Page Numbering
+//define("aliasNbPg","{nb}");
+//define("aliasNbPgGp","{nbpg}");
+//define("pagenumPrefix","");
+//define("pagenumSuffix","");
+//define("nbpgPrefix","");
+//define("nbpgSuffix","");
+//define("defaultPageNumStyle","1");
+#endregion
+
+#region Fonts, Languages and Charcter Sets
+//define("percentSubset",30);
+//define("useKerning",false);
+//define("maxTTFFilesize",2000);
+//define("allow_charset_conversion",true);
+//define("charset_in",null);
+//define("biDirectional",false);
+//define("useAdobeCJK",true);
+//define("autoScriptToLang",false);
+//define("autoLangToFont",false);
+//define("baseScript",1);
+//define("autoArabic",false);
+//define("autoVietnamese",false);
+//define("falseBoldWeight",5);
+//define("smCapsScale",0.75);
+//define("smCapsStretch",115);
+//define("backupSubsFont",array('dejavusanscondensed'));
+//define("backupSIPFont",'');
+#endregion
+
+#region Configuration
+//define("repackageTTF",false);
+//define("useSubstitutions",false);
+//define("collapseBlockMargins",true);
+//define("dpi",96);
+//define("enableImports",false);
+//define("allow_output__buffering",false);
+//define("allow_html_optional_endtags",true);
+//define("ignore_invalid_utf8",false);
+//define("text_input_as_HTML",false);
+//define("progressBar",false);
+//define("progbar_heading","mPDF file progress");
+//define("progbad_altHTML","");
+//define("incrementFPR1 [1-4]",10,20,30,50);
+#endregion
+
+#region Debugging
+//define("debug",false);
+//define("debugfonts",false);
+//define("showImageErrors",false);
+#endregion
+
+#region PDF/A1-b, PDF/X-1a Colorspaces
+//define("PDFA",false);
+//define("PDFAauto",false);
+//define("PDFX",false);
+//define("PDFXauto",false);
+//define("ICCProfile","");
+//define("restrictColorSpace",0);
+#endregion
+
+#region Annotations
+//define("title2annots",false);
+//define("annotMargin",null);
+//define("annotOpacity",0.5);
+#endregion
+
+#region Bookmarks (Outlines)
+//define("anchor2Bookmark",0);
+//define("h2bookmarks",array());
+//define("h2toc",array());
+#endregion
+
+#region Css & Styles
+//define("CSSselectMedia","print");
+#endregion
+
+#region Page Headers & Footers
+//define("defaultfooterfontstyle","BI");
+//define("defaultfooterline",1);
+//define("defaultheaderfontsize",8);
+//define("defaultheaderfontstyle","BI");
+//define("defaultheaderline",1);
+//define("footer_line_spacing",0.25);
+//define("header_line_spacing",0.25);
+//define("forcePortraitHeaders",false);
+#endregion
+
+#region Tables
+//define("simpleTables",false);
+//define("packTableData",false);
+//define("tableMinSizePriority",false);
+//define("ignore_table_percents",false);
+//define("ignore_table_widths",false);
+//define("keep_table_proportions",false);
+//define("shrink_tables_to_fit",1.4);
+//define("table_error_report",false);
+//define("table_error_report_param","");
+//define("use_kwt",false);
+//define("iterationCounter",false);
+//define("decimal_align",array());
+#endregion
+
+#region Images
+//define("img_dpi",96);
+#endregion
+
+#region Text Spacing & Justification
+//define("normalLineheight",1.33);
+//define("useFixedNormalLineHeight",false);
+//define("useFixedTextBaseline",false);
+//define("adjustFontDescLineheight",1.14);
+//define("jSmaxChar",2);
+//define("jSmaxWordLast",2);
+//define("jSmaxCharLast",1);
+//define("jSWord",0.4);
+//define("allowCJKorphans",true);
+//define("allowCJKoverflow",false);
+//define("CJKforceend",false);
+//define("tabSpaces",8);
+//define("justifyB4br",false);
+#endregion
+
+#region Hyphenation
+//define("SHYlang","en");
+#endregion
+
+#region Columns
+//define("keepColumns",false);
+//define("max_colH_correction",1.15);
+#endregion
+
+#region Lists
+//define("list_auto_mode","browser");
+//define("list_indent_default","40px");
+//define("list_indent_default_mpdf","0em");
+//define("list_marker_offset","5.5pt");
+//define("list_symbol_size","3.6pt");
+//define("list_indent_first_level",0);
+//define("list_number_suffix",".");
+#endregion
+
+#region Watermarks
+//define("showWatermarkImage",null);
+//define("showWatermarkText",null);
+//define("watermark_font","");
+//define("watermarkImageAlpha",0.2);
+//define("watermarkImgAlphaBlend","Normal");
+//define("watermarkImgBehind",false);
+//define("watermarkTextAlpha",0.2);
+#endregion
+
+#region Borders
+//define("autoPadding",false);
+#endregion
+
+#region Bookmarks
+//define("bookmarkStyles",array());
+#endregion
